98278a6b19eb2e5b5737ed085d18b696273ce3f3
RifaAndhika259 <rifaandhika25@gmail.com>
2021-10-18 10:48:35 +0700

    first commit

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,80 @@
+
+  <!--Start sidebar-wrapper-->
+  <div id="sidebar-wrapper" data-simplebar="" data-simplebar-auto-hide="true">
+    <div class="brand-logo">
+     <a href="index.html">
+      <img src="assets/images/logo-icon.png" class="logo-icon" alt="logo icon">
+      <h5 class="logo-text">RockerAdmin</h5>
+    </a>
+    </div>
+    <ul class="sidebar-menu do-nicescrol">
+     <li class="sidebar-header">MAIN NAVIGATION</li>
+     <li>
+       <a href="index.html" >
+         <i class="icon-home"></i> <span>Dashboard</span>
+       </a>
+
+     </li>
+
+
+
+     <li>
+       <a href="1" >
+         <i class="icon-layers"></i>
+         <span>Product</span>
+
+       </a>
+
+     </li>
+
+
+     <li>
+        <a href="1" >
+          <i class="icon-user"></i>
+          <span>Custumer</span>
+
+        </a>
+
+      </li>
+
+
+
+
+      <li>
+       <a href="javaScript:void();" class="waves-effect">
+         <i class="icon-grid"></i> <span>Transaksi</span>
+         <i class="fa fa-angle-left float-right"></i>
+       </a>
+       <ul class="sidebar-submenu">
+         <li>
+             <a href="table-simple-tables.html"><i class="fa fa-circle-o"></i> Transaksi</a></li>
+             <li><a href="table-header-tables.html"><i class="fa fa-circle-o"></i> List Transaksi</a></li>
+             <li><a href="table-color-tables.html"><i class="fa fa-circle-o"></i> Riwayat Transaksi</a></li>
+      </ul>
+      </li>
+
+
+
+      <li>
+        <a href="2">
+          <i class="icon-settings"></i> <span>Setting</span>
+
+        </a>
+
+      </li>
+
+
+      <li>
+        <a href="javaScript:void();" class="waves-effect">
+          <i class="icon-user"></i> <span>Manajement User</span>
+          <i class="fa fa-angle-left float-right"></i>
+        </a>
+        <ul class="sidebar-submenu">
+          <li>
+              <a href="table-simple-tables.html"><i class="fa fa-circle-o"></i> Pengguna</a></li>
+              <li><a href="table-header-tables.html"><i class="fa fa-circle-o"></i> Hak Akses</a></li>
+
+       </ul>
+       </li>
+  </div>
+  <!--End sidebar-wrapper-->
